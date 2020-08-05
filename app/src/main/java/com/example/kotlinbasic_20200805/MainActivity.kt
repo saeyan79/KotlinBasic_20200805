85a6a2d9d3635ba6a3db063e89e32f8c0ad73b9a
박새 <yannn7979@gmail.com>
2020-08-05 20:49:02 +0900

    두번째 버튼 추가 + 이벤트 처리

--- app/src/main/java/com/example/kotlinbasic_20200805/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic_20200805/MainActivity.kt
@@ -14,11 +14,17 @@ class MainActivity : AppCompatActivity() {
 //        메모처럼 주석을 적자
 //        xml에 그려둔 clickme 버튼 눌렀을때 감지하기
 
-    clickMeBtn.setOnClickListener {
+        clickMeBtn.setOnClickListener {
 //        지금 코드에 회색 바탕으로 나오는 것들은 자동 입력되는것으로 신경 쓸 필요 없음. 따라적지도 마
 //        이 중괄호 내부의 코드 => clickMeBtn 눌리면 실행 될 코드 적는 곳
-        Log.d("버튼누름", "눌렸다!")
-    }
+        Log.d("버튼누름", "Click me 눌렸다!")
+        }
+
+
+        secondBtn.setOnClickListener {
+//        두번째 버튼 눌렸을 ㄸㅐ 실행 될 코드
+            Log.d("버튼누름", "두번째 버튼 눌렸다!")
+        }
 
     }
 }
